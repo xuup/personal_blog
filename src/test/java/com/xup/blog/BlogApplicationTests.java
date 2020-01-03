1be910789c2ee7b46740879f2922824851568e4f
xuup <770585725@qq.com>
2020-01-03 17:40:34 +0800

    初始化blog Spring Boot框架

---
+++ src/test/java/com/xup/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xup.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
